5a5c165c9a60fa294bf9edde14127dde73c1a2b2
israelwerther <israelwerther48@outlook.com>
2024-10-19 15:20:43 -0300

    chore: install djangorestframework

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My Apps
     'socialmedia.core'
+
+    # API
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
