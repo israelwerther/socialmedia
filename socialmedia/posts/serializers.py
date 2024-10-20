d522f7ecc54460313b0abfe2c37abc7d217190c5
israelwerther <israelwerther48@outlook.com>
2024-10-20 15:20:29 -0300

    feat: list and create posts

--- socialmedia/posts/serializers.py
+++ socialmedia/posts/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source='user.username', read_only=True) 
     class Meta:
         model = Post
         fields = ['id', 'user', 'content', 'created_at']
