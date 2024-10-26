c78ad5c2292e0e94ecba98e0e8b9922c061f21ad
israelwerther <israelwerther48@outlook.com>
2024-10-26 18:13:14 -0300

    fix: fix redirect url for login

--- socialmedia/core/views.py
+++ socialmedia/core/views.py
@@ -4,5 +4,4 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'core/index.html'
-    login_url = '/login/'
-    redirect_field_name = 'redirect_to'
+    login_url = 'contas/login/'
