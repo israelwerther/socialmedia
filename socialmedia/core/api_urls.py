f13657b8a1f84b259ff891508d68f9dcda320d1a
israelwerther <israelwerther48@outlook.com>
2024-10-27 16:49:03 -0300

    refactor: change api to posts apis.py

--- socialmedia/core/api_urls.py
+++ socialmedia/core/api_urls.py
@@ -4,7 +4,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 from socialmedia.accounts.apis import UserViewSet
-from socialmedia.posts.views import PostViewSet
+from socialmedia.posts.apis import PostViewSet
 
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
